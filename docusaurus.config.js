beb7a6f6239139a6a8a5f48bba470e46c01fb8d0
NoahMaizels <noahmaizels@protonmail.com>
2026-05-19 16:48:29 +0700

    switch algolia index

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,11 +78,11 @@ module.exports = {
       copyright: `Copyleft © ${new Date().getFullYear()}.`,
     },
     algolia: {
-      appId: "D7BASQ1ESY",
+      appId: "ZC4FF0H3GZ",
 
-      apiKey: "8a175ff0c34022d46b4b26cf6f02ad41",
+      apiKey: "70752ba60034b0dcce5cc5888f2fd267",
 
-      indexName: "bee-js-docs-search",
+      indexName: "bee-js-docs-2026",
 
       contextualSearch: false,
     },
